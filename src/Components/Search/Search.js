fe54cba14d71a784c13d7b63023b2f29ba6610a9
dvimukthi <deshanvimukthi59@gmail.com>
2022-10-06 23:22:38 +0530

    AsyncPaginate

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <AsyncPaginate
+      placeholder="Search For City"
+      debounceTimeout={600}
+      value={Search}
+    />
+  );
 };
 
 export default Search;
