dba0a56cbf2c76aa986873e3dcb5b940486dad9c
dvimukthi <deshanvimukthi59@gmail.com>
2022-10-06 23:29:19 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import "./App.css";
 import Search from "./Components/Search/Search";
 
 function App() {
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  };
+
   return (
     <div className="Container">
-      <Search />
+      <Search onSearchChange={handleOnSearchChange} />
     </div>
   );
 }
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 
-const Search = () => {
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
   return (
     <AsyncPaginate
       placeholder="Search For City"
       debounceTimeout={600}
       value={Search}
+      onChange={handleOnChange}
     />
   );
 };
