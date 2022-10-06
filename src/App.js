96547a20eb0841752c1ccecaa7e157666c73c6f8
dvimukthi <deshanvimukthi59@gmail.com>
2022-10-06 21:36:46 +0530

    search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Search from "./Components/Search/Search";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Search />
+    </div>
+  );
 }
 
 export default App;
