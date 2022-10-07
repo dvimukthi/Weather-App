293a461136441b2e396227dec618e60abdf4546c
dvimukthi <deshanvimukthi59@gmail.com>
2022-10-07 23:32:43 +0530

    Icons

--- src/Components/Current-Weather/CurrentWeather.js
+++ src/Components/Current-Weather/CurrentWeather.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./CurrentWeather.css";
 
 const CurrentWeather = () => {
-  return <div>Current-Weather</div>;
+  return (
+    <div className="weather">
+      <div className="top">
+        <p className="City">Belgrade</p>
+        <p className="weatherDescription">Sunny</p>
+      </div>
+      <img alt="weather" className="weather-icon" src="icons/01d.png" />
+    </div>
+  );
 };
 
 export default CurrentWeather;
