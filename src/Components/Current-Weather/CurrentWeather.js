a6e4350a84147bb292d58fa662c2d718167e09ec
dvimukthi <deshanvimukthi59@gmail.com>
2022-10-08 22:00:39 +0530

    weather data

--- src/Components/Current-Weather/CurrentWeather.js
+++ src/Components/Current-Weather/CurrentWeather.js
@@ -16,26 +16,28 @@ const CurrentWeather = ({ data }) => {
         />
       </div>
       <div className="bottom">
-        <p className="temperature">18°C</p>
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
         <p className="details">
           <div className="parameter-row">
             <span className="parameter-label">Details</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Feels Like</span>
-            <span className="parameter-value">22</span>
+            <span className="parameter-value">
+              {Math.round(data.main.feels_like)}°C
+            </span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
-            <span className="parameter-value">2 m/s</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
-            <span className="parameter-value">20%</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Pressure</span>
-            <span className="parameter-value">15 hpa</span>
+            <span className="parameter-value">{data.main.pressure} hpa</span>
           </div>
         </p>
       </div>
