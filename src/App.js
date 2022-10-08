51db93549291a2ec32a3e52db33bf2ce96e4c93a
dvimukthi <deshanvimukthi59@gmail.com>
2022-10-08 20:56:03 +0530

    currentWeatherFetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./Api";
 import "./App.css";
 import CurrentWeather from "./Components/Current-Weather/CurrentWeather";
 import Search from "./Components/Search/Search";
@@ -8,7 +9,7 @@ function App() {
     const [lat, lon] = searchData.value.split(" ");
 
     const currentWeatherFetch = fetch(
-      `/weather?lat={lat}&lon={lon}&appid={API key}`
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
     );
   };
 
